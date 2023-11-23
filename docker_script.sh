2e996d55bd9517d63da8c8718eaafbe6ec02debc
Ethan <ethanmccaskey2@gmail.com>
2023-11-22 18:21:48 -0700

    Single Model

--- docker_script.sh
+++ docker_script.sh
@@ -2,5 +2,5 @@
 
 docker run --gpus all --rm -it -p 8888:8888 -p 8787:8787 -p 8786:8786 \
     -v "$(pwd):/rapids/notebooks/host" \
-    --cpus=6 --memory="16g" \
+    --cpus=6 --memory="24g" \
     rapidsai/rapidsai:23.06-cuda11.8-runtime-ubuntu22.04-py3.10
